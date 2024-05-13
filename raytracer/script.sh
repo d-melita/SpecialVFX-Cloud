fe33a6834561bb88f5e8873bf8d9dc912defd35b
diogo antunes <diogo.santiago.pt@gmail.com>
2024-05-13 11:10:39 +0100

    feat: updated scripts
    - extended raytracer script to support optional texture and more params
    - added script to automatically load server

--- raytracer/script.sh
+++ raytracer/script.sh
@@ -1,7 +1,8 @@
+
 #!/usr/bin/env bash
 
 function print_usage() {
-    printf "Usage: $0 <input-file.txt> <texture-file.bmp> <output-file.bmp>\n"
+    printf "Usage: $0 <input-file.txt> <output-file.bmp> [<texture-file.bmp>]\n"
 }
 
 function add_scene_to_json() {
@@ -14,14 +15,26 @@ function add_scene_to_json() {
 
 function add_texture_to_json() {
     local texture_file=$1
-    if ! hexdump -ve '1/1 "%u\n"' "$texture_file" | jq -s --argjson original "$(cat resources/payload.json)" '$original * {texmap: .}' > resources/payload.json; then
-        printf "Failed to add texture data to JSON.\n" >&2
-        return 1
+    if [[ -f "$texture_file" ]]; then
+        if ! hexdump -ve '1/1 "%u\n"' "$texture_file" | jq -s --argjson original "$(cat resources/payload.json)" '$original * {texmap: .}' > resources/payload.json; then
+            printf "Failed to add texture data to JSON.\n" >&2
+            return 1
+        fi
+    else
+        printf "No texture file provided. Skipping texture data addition to JSON.\n"
     fi
 }
 
 function send_request() {
-    if ! curl -X POST "http://127.0.0.1:8000/raytracer?scols=400&srows=300&wcols=400&wrows=300&coff=0&roff=0&aa=false" --data "@./resources/payload.json" > resources/result.txt; then
+    local scols=$1
+    local srows=$2
+    local wcols=$3
+    local wrows=$4
+    local coff=$5
+    local roff=$6
+
+    echo "Requesting to: http://127.0.0.1:8000/raytracer?scols=$scols&srows=$srows&wcols=$wcols&wrows=$wrows&coff=$coff&roff=$roff&aa=false"
+    if ! curl -X POST "http://127.0.0.1:8000/raytracer?scols=$scols&srows=$srows&wcols=$wcols&wrows=$wrows&coff=$coff&roff=$roff&aa=false" --data "@./resources/payload.json" > resources/result.txt; then
         printf "Failed to send request to the server.\n" >&2
         return 1
     fi
@@ -43,18 +56,24 @@ function decode_base64() {
 }
 
 function main() {
-    if [[ $# -ne 3 ]]; then
+    if [[ $# -lt 8 || $# -gt 9 ]]; then
         print_usage
         return 1
     fi
 
     local input_file=$1
-    local texture_file=$2
-    local output_file=$3
+    local output_file=$2
+    local scols=$3
+    local srows=$4
+    local wcols=$5
+    local wrows=$6
+    local coff=$7
+    local roff=$8
+    local texture_file=$9
 
     add_scene_to_json "$input_file" || return 1
     add_texture_to_json "$texture_file" || return 1
-    send_request || return 1
+    send_request $scols $srows $wcols $wrows $coff $roff || return 1
     remove_formatting || return 1
     decode_base64 "$output_file" || return 1
 
@@ -62,4 +81,3 @@ function main() {
 }
 
 main "$@"
-
