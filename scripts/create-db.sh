40045d30b6d2e7302964e9a53917a7172990c853
Diogo Melita <dgmelita9@gmail.com>
2024-05-28 22:40:07 +0100

    feat: add dynamoDB scripts to create and delete

---
+++ scripts/create-db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Delete table if already exists
+./delete-db.sh
+
+echo "Creating the table"
+aws dynamodb create-table \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME \
+  --attribute-definition \
+    AttributeName=RequestParams,AttributeType=S \
+  --key-schema \
+    AttributeName=RequestParams,KeyType=HASH \
+  --provisioned-throughput \
+    ReadCapacityUnits=1,WriteCapacityUnits=1 \
+  --table-class STANDARD 2>&1 | jq .
+
+echo "Waiting until table is available"
+aws dynamodb wait table-exists \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME
---
+++ scripts/delete-db.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Delete table if already exists
+echo "Deleting any pre-existing table with the same name"
+aws dynamodb delete-table \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME 2>> /dev/null
+
+echo "Waiting until table is deleted"
+aws dynamodb wait table-not-exists \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME
